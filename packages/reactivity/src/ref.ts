1dbecb47944c0b5fb2f3c9a5af4073eadfba8834
windowdotonload <275981471@qq.com>
2021-07-09 16:35:50 +0800

    mod

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,6 +1,7 @@
-import { hasChange } from "@vue/shared"
+import { hasChange, isObject } from "@vue/shared"
 import { track, trigger } from "./effect"
 import { TrackOpTypes, TriggerOrTypes } from './operator'
+import { reactive } from "./reactive"
 
 /*
  * @Descripttion:
@@ -17,11 +18,12 @@ export function shallowRef(value) {
     return createRef(value, true)
 }
 
+const convert = (val) => isObject(val) ? reactive(val) : val
 class RefImpl {
     public _value
     public __v_isRef = true
     constructor(public rawValue, public shallow) {
-        this._value = rawValue
+        this._value = shallow ? rawValue : convert(rawValue)
     }
 
     get value() {
@@ -33,12 +35,30 @@ class RefImpl {
     set value(newValue) {
         if (hasChange(newValue, this.rawValue)) {
             this.rawValue = newValue
-            this._value = newValue
+            this._value = this.shallow ? newValue : convert(newValue)
             trigger(this, TriggerOrTypes.SET, 'value', newValue)
         }
     }
 }
 
+class ObjectRefImpl {
+    public __v_isRef = true
+    public _value
+    constructor(public target, public key) {
+
+    }
+    get value() {
+        return this.target[this.key]
+    }
+    set value(newValue) {
+        this.target[this.key] = newValue
+    }
+}
+
 function createRef(rawValue, shallow = false) {
     return new RefImpl(rawValue, shallow)
+}
+
+export function toRef(target, key) {
+    return new ObjectRefImpl(target, key)
 }
